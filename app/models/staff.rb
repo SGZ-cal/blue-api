962dbc2b4aae7e5ba5ee9f71db5d4d468e97b6d0
sgz-cal <ls16012j@gmail.com>
2020-10-24 08:35:22 +0900

    add_email_custom_validation

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,11 +1,19 @@
+require "validator/email_validator"
+
 class Staff < ApplicationRecord
   # gem bcrypt
   has_secure_password
+
+  before_validation :downcase_email
 
   # validates
   validates :name,
     presence: true,
     length: { maximum: 30, allow_blank: true }
+
+  validates :email,
+    presence: true,
+    email: { allow_blank: true }
 
   VALID_PASSWORD_REGEX = /\A[\W\-]+\z/
   validates :password,
@@ -16,4 +24,27 @@ class Staff < ApplicationRecord
       message: :invalid_password
     },
     allow_blank: true
+
+  ## methods
+  # class method #############
+  class << self
+    # emailからアクティブなユーザーを返す
+    def find_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+  # class method end #############
+
+  # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
+  def email_activated?
+    staffs = Staff.where.not(id: id)
+    staffs.find_activated(email).present?
+  end
+
+  private
+
+    # email小文字化
+    def downcase_email
+      self.email.downcase! if email
+    end
 end
